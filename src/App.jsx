960336ff1a724a39860f1cccf9a33cc475981b33
Jonas <jonashalvorsen2013@live.no>
2024-10-01 09:48:35 +0200

    Add-context

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, createContext  } from 'react'
 import Header from './components/Header'
 import Tweets from './components/Tweets'
 import RightSide from './components/RightSide'
 import defaultTweets from './assets/data/tweets.js'
 import user from './assets/data/user.js'
+export const TwitterContext = createContext()
 
 function App() {
     const [tweets, setTweets] = useState(defaultTweets)
@@ -17,11 +18,27 @@ function App() {
 
     return (
         <div className="container">
-            <Header user={user} theme={theme} setTheme={setTheme} />
-            <Tweets tweets={tweets} setTweets={setTweets} user={user} theme={theme}  />
-            <RightSide theme={theme} />
+            <TwitterContext.Provider
+            value={{ 
+                tweets: tweets, 
+                setTweets: setTweets, 
+                theme: theme, 
+                setTheme: setTheme,
+                user: user }}
+            >
+                <Header/>
+                <Tweets/>
+                <RightSide/>
+            </TwitterContext.Provider>
         </div>
     )
+    // return (
+    //     <div className="container">
+    //         <Header user={user} theme={theme} setTheme={setTheme} />
+    //         <Tweets tweets={tweets} setTweets={setTweets} user={user} theme={theme}  />
+    //         <RightSide theme={theme} />
+    //     </div>
+    // )
 }
 
 export { App };
--- src/components/Tweets.jsx
+++ src/components/Tweets.jsx
@@ -1,20 +1,24 @@
+import { useContext } from 'react'
 import CreateTweet from './CreateTweet'
 import Tweet from './Tweet'
+import { TwitterContext } from '../App'
 
-export default function Tweets({ tweets, setTweets, user, theme }) {
-  return (
-        <main>
-            <div className={theme === 'dark' ? 'top-bar dark' : 'top-bar'}>
-                <h2 className="title">Home</h2>
-            </div>
+export default function Tweets() {
+    const { tweets, theme } = useContext(TwitterContext)
 
-            <CreateTweet tweets={tweets} setTweets={setTweets} user={user} theme={theme} />
+    return (
+            <main>
+                <div className={theme === 'dark' ? 'top-bar dark' : 'top-bar'}>
+                    <h2 className="title">Home</h2>
+                </div>
 
-            <div className="show-more-tweets">
-                <p>Show 35 Tweets</p>
-            </div>
+                <CreateTweet />
 
-            {tweets.map((tweet, index) => <Tweet tweet={tweet} theme={theme} key={index} />)}
-        </main>
-    )
+                <div className="show-more-tweets">
+                    <p>Show 35 Tweets</p>
+                </div>
+
+                {tweets.map((tweet, index) => <Tweet tweet={tweet} key={index} />)}
+            </main>
+        )
 }
